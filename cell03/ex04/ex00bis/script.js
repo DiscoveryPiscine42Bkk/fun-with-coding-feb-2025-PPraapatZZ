9e50dcc346ac0f4dd90c083329b92588fd3be709
Pearapat <138387155+PPraapatZZ@users.noreply.github.com>
2025-02-06 23:48:46 +0700

    Update background color generation and integrate calculator functionality into HTML

--- cell03/ex04/ex00bis/script.js
+++ cell03/ex04/ex00bis/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
     $("#changeBtn").click(function() {
         // เปลี่ยนสีพื้นหลังแบบสุ่ม
-        let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        let randomColor = "#" + Math.floor(Math.random() * 255 * 255 * 255).toString(16);
         $("body").css("background-color", randomColor);
 
         // ทำให้ปุ่มใหญ่ขึ้นเรื่อยๆ
